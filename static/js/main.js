e34432d7dd1a03c65664a0492c237e55d89d2ac1
Daniel Ewert <ewert.daniel@gmail.com>
2016-02-13 14:38:33 +0100

    working version, no persistance

---
+++ static/js/main.js
@@ -0,0 +1,60 @@
+var editor_head = "<textarea id='editor' class='form-control' rows=8>";
+var editor_foot = "</textarea>";
+var editor = editor_head + editor_foot;
+var element_head = "<div class='row'><div class='col-md-12'>";
+var element_foot = "</div></div>";
+var id = 0;
+var entry_db = {};
+
+
+jQuery.fn.extend({
+  markify: function(){
+    var content = $(this).val();
+    if ($.trim(content)!==""){
+        var entry_id = parseInt($(this).parent().parent().attr("id"));
+        entry_db[entry_id]=content;
+        $(this).replaceWith("<div class='entry'>" +  marked(content)  + "</div>");
+      } else {
+        $(this).parent().parent().remove();
+      }
+    $("#plusbutton").show();
+  }
+});
+
+
+$(document).ready(function(){
+  console.log("loaded page");
+  
+  $(".entry").each(function(){
+    $(this).html(marked($(this).text()));
+  });
+  
+  $("#plusbutton").click(function(){
+    $(this).after(element_head + editor + element_foot).next().attr("id", id++);
+    $(this).hide();
+    $("#editor").focus();
+  });
+  
+  $("body").on("click", ".entry", function(){
+    $("#plusbutton").hide();
+    var entry_id = parseInt($(this).parent().parent().attr("id"));
+    console.log(entry_id);
+    console.log(entry_db);
+    $(this).replaceWith(editor);
+    $("#editor").focus().val(entry_db[entry_id]);
+    
+  });
+  
+  $("body").on('focusout', "#editor",function(){
+    $(this).markify();
+  });
+  
+  $("body").on('change keyup paste',"#editor", function() {
+    var content = $(this).val();
+    if (content.endsWith("\n\n")){
+      $(this).val(content.slice(0,-2));
+      $(this).markify();
+    }
+});
+ 
+});
