5a19e8766afd1806aa170866164cf098c9158258
Daniel Ewert <ewert.daniel@gmail.com>
2016-02-13 21:18:41 +0100

    fix double event trigger

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 var editor_head = "<textarea id='editor' class='form-control' rows=8>";
 var editor_foot = "</textarea>";
-var editor = editor_head + editor_foot;
+var editor_html = editor_head + editor_foot;
 var element_head = "<div class='row'><div class='col-md-12'>";
 var element_foot = "</div></div>";
 var id = 0;
@@ -12,6 +12,7 @@ jQuery.fn.extend({
     var content = $(this).val();
     if ($.trim(content)!==""){
         var entry_id = parseInt($(this).parent().parent().attr("id"));
+        console.log("adding " + entry_id + ": " + content);
         entry_db[entry_id]=content;
         $(this).replaceWith("<div class='entry'>" +  marked(content)  + "</div>");
       } else {
@@ -23,14 +24,13 @@ jQuery.fn.extend({
 
 
 $(document).ready(function(){
-  console.log("loaded page");
   
   $(".entry").each(function(){
     $(this).html(marked($(this).text()));
   });
   
   $("#plusbutton").click(function(){
-    $(this).after(element_head + editor + element_foot).next().attr("id", id++);
+    $(this).after(element_head + editor_html + element_foot).next().attr("id", id++);
     $(this).hide();
     $("#editor").focus();
   });
@@ -38,27 +38,22 @@ $(document).ready(function(){
   $("body").on("click", ".entry", function(){
     $("#plusbutton").hide();
     var entry_id = parseInt($(this).parent().parent().attr("id"));
-    console.log(entry_id);
-    console.log(entry_db);
-    $(this).replaceWith(editor);
+    $(this).replaceWith(editor_html);
     $("#editor").focus().val(entry_db[entry_id]);
-    
   });
-  
-  $("body").on('focusout', "#editor",function(){
+
+
+  $("body").on('focusout','#editor', function(){
     $(this).markify();
-  });
-  
+  }); 
   
   $("body").on('change keyup paste',"#editor", function(e) {
     var content = $(this).val();
     if (e.keyCode == 27) {
-      $(this).blur();
-      $(this).markify();
+      $(this).blur(); //triggers focusout
     } else if (content.endsWith("\n\n\n")){
       $(this).val(content.slice(0,-3));
-      $(this).blur();
-      $(this).markify();
+      $(this).blur(); //triggers focusout
     }
 });
  
